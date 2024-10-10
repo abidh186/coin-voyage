271640c502f7996d7ccbacc36a85540155b9eb15
abidh186 <abidhussain@pursuit.org>
2024-10-10 14:20:07 -0400

    feat (header): add new header with links to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomeComponent from '../app/components/home';
+import HeaderComponent from '../app/components/header'; // Adjust the path as necessary
 
 const Home: React.FC = () => {
   return (
     <div>
+      <HeaderComponent />
       <HomeComponent />
     </div>
   );
